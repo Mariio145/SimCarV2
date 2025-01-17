68c6c9c997ed4d75f479b488798ec4b062e0e930
Mario Beltran Garcia <mario20032001@gmail.com>
2025-01-17 20:35:30 +0100

    Error aleatoriedad

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private TMP_InputField seedText;
     public void ActivateScene(string sceneName)
     {
+        if (seedText.text == "")
+        {
+            GlobalVariables.Seed = Random.Range(0, 10000);
+        }
         SceneManager.LoadScene(sceneName); // Activa la escena cuando esté lista
     }
 }
